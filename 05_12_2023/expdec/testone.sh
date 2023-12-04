cade9f64436a998a2a22f0eaeb045257f5a79aa5
Artur Gaspar <arthur.gaspar2011@gmail.com>
2023-12-04 16:22:00 -0300

    Added everything except plot of 05_12_2023 exponential decay experiment, also checked a bit if it's ok.

--- 05_12_2023/expdec/testone.sh
+++ 05_12_2023/expdec/testone.sh
@@ -1,11 +1,16 @@
-#!/bin/sh
+#!/bin/sh -x
 
 # Computes exponential decay with alpha^i from i=1 to i=n, using the n choices provided.
 
-alfa=$1;
+if test \( "$#" -ne 7 \) -o \( ! -f "$5" \) -o \( ! -f "$6" \) -o \( ! -f "$7" \) -o \( -z "$1" \) -o \( -z "$2" \) -o \( -z "$3" \) -o \( -z "4" \); then
+  echo "Usage: $0 <alpha> <number of elements in exponential decay> <choices to make: 0 for not-strategy, 1 for strategy> <number of simulation, for naming purposes, should reflect the choices: choices 0000 implies number 0, choices 1000 implies number 1, choices 0100 implies number 2, and so on> <prefile.kf> <channel.kf> <strategy.kf>" >&2
+  exit 1
+fi
+
+alpha=$1;
 n=$2;
 choices=$3; # As one string, space separated!
-number=$4; # For naming purposes. 0000 should be 0, 0010 should be 2.
+number=$4; # For naming purposes. 0000 should be 0, 1000 should be 1, 0100 should be 2!
 prefile=$5;
 chanfile=$6
 stratfile=$7
@@ -25,7 +30,7 @@ concat_code() {
     fi
 }
 
-echo '' > /tmp/vulns$number.txt
+echo -n '' > /tmp/vulns$number.txt
 
 npre=$(cat /tmp/pre.txt | wc -l);
 i=0;
@@ -43,6 +48,9 @@ for choice in $(cat /tmp/pre.txt) $choices; do # 0 == not strat, 1 == strat
         if test $chans -ne 0; then
             concat_code 0 $chans;
         fi;
+        if test $i -eq $npre; then
+            cp /tmp/sim$number.kf /tmp/simini$number.kf
+        fi
         cabal new-run Kuifje-compiler /tmp/sim$number.kf > /tmp/sim$number.txt
         cp /tmp/simtmp$number.kf /tmp/sim$number.kf
         rm /tmp/simtmp$number.kf
@@ -62,13 +70,16 @@ else
     echo "Something went wrong... Should have $n choices but only $(expr $i - $npre) were given"
 fi
 
-#val=$(echo $alpha $n "/tmp/vulns$number.txt" | python3 getval.py)
-#vul1=$(cat /tmp/vulns$number.txt | head -1)
+val=$(echo "$alpha"\\n"$n"\\n"/tmp/vulns$number.txt" | python3 getval.py)
+vul1=$(cat /tmp/vulns$number.txt | head -1)
+echo "$val $vul1 $number $n $choices" >> /tmp/vulnsall.txt
 
 
 rm /tmp/vulns$number.txt
 # rm /tmp/sim$number.kf
+# rm /tmp/simini$number.kf
 # rm /tmp/sim$number.txt
+# rm /tmp/vulnsall.txt
 # Remove these after running all.
 
 # /tmp/pre.txt should exists and either be empty or have one number (0 or 1) per line.
